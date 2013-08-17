1f2953eaa1e4352c448fedd90ef6ff48abdbdb0f
Emre Yılmaz <mail@emreyilmaz.me>
2013-08-17 14:26:04 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='storm-indicator',
-    version='0.1',
+    version='0.2',
     packages=['storm_indicator'],
     url='https://github.com/emre/storm-indicator',
     license='MIT',
